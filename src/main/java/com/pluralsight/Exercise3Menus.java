c2e03964dbdcb928913cb500a81d9e96781f372c
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-12-02 10:20:42 -0500

    * Exercise3 updated to include switch statement and try-catch-finally

--- src/main/java/com/pluralsight/Exercise3Menus.java
+++ src/main/java/com/pluralsight/Exercise3Menus.java
@@ -4,30 +4,47 @@ import java.sql.*;
 import java.util.Scanner;
 
 public class Exercise3Menus {
-    public static void main(String[] args) {
-        String url = "jdbc:mysql://127.0.0.1:3306/northwind";
-        String user = "root";
-        String password = "yearup";
-        Scanner keyboard = new Scanner(System.in);
+    public static final String url = "jdbc:mysql://127.0.0.1:3306/northwind";
+    public static final String user = "root";
+    public static final String password = "yearup";
+    public static final Scanner keyboard = new Scanner(System.in);
 
+    public static void main(String[] args) {
         System.out.println("What table do you want to see?");
         System.out.println("1) Display All Product Data");
         System.out.println("2) Display ALL Customer Data");
+        System.out.println("99) Exit Program");
         System.out.print("Enter input here: ");
         int userInput = keyboard.nextInt();
         keyboard.nextLine();
 
-        if (userInput == 1) {
+        switch (userInput) {
+            case 1 -> productData();
+            case 2 -> customerData();
+            case 99 -> {
+                keyboard.close();
+                System.exit(0);
+            }
+            default -> System.out.println("Please enter a valid input.");
+        }
+    }
+
+        public static void productData() {
             String query = "SELECT ProductID, ProductName, UnitPrice, UnitsInStock " +
                     "FROM Products WHERE ProductID = ? OR ProductName LIKE ?";
-            try {
-                Connection connection = DriverManager.getConnection(url, user, password);
-                PreparedStatement statement = connection.prepareStatement(query);
 
-                statement.setInt(1, 1); //auto sanitize mal inputs
+            ResultSet results = null;
+            PreparedStatement statement = null;
+            Connection connection = null;
+
+            try {
+                connection = DriverManager.getConnection(url, user, password);
+                statement = connection.prepareStatement(query);
+
+                statement.setInt(1, 1);
                 statement.setString(2, "%%");
 
-                ResultSet results = statement.executeQuery();
+                results = statement.executeQuery();
 
                 System.out.println("ProductID \t ProductName \t UnitPrice \t UnitsInStock");
                 System.out.println("-----------------------------------------------------------");
@@ -37,49 +54,89 @@ public class Exercise3Menus {
                     int unitPrice = results.getInt("UnitPrice");
                     int unitStock = results.getInt("UnitsInStock");
                     System.out.println(prodID + "\t" + prodName + "\t" + unitPrice + "\t" + unitStock);
-
                 }
-                results.close();
-                statement.close();
-                connection.close();
-            } catch (SQLException e) {
+            }
+            catch (SQLException e) {
                 e.printStackTrace();
             }
-        }
-        else if (userInput == 2) {
-            String query = "SELECT CompanyName, ContactName, City, Country, Phone " +
-                    "FROM Customers WHERE CustomerID = ? OR CompanyName LIKE ? " +
-                    "ORDER BY Country";
-            try {
-                Connection connection = DriverManager.getConnection(url, user, password);
-                PreparedStatement statement = connection.prepareStatement(query);
-
-                statement.setInt(1, 1); //auto sanitize mal inputs
-                statement.setString(2, "%%");
-
-                ResultSet results = statement.executeQuery();
-                // CustomerID, ContactName, CompanyName, City, Country, Phone
-                System.out.println("CustomerID \t ContactName \t CompanyName \t City \t Country \t Phone");
-                System.out.println("-----------------------------------------------------------");
-                while (results.next()) {
-                    String contactName = results.getString("ContactName");
-                    String companyName = results.getString("CompanyName");
-                    String city = results.getString("City");
-                    String country = results.getString("Country");
-                    String phone = results.getString("Phone");
-                    System.out.println(contactName + "\t" + companyName +
-                            "\t" + city + "\t" + country + "\t" + phone);
-
+            finally {
+                if (results != null) {
+                    try {
+                        results.close();
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
                 }
-                results.close();
-                statement.close();
-                connection.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
+                if (statement != null) {
+                    try {
+                        statement.close();
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                }
+                if (connection != null) {
+                    try {
+                        connection.close();
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                }
             }
         }
-        else {
-            System.out.println("Enter in a correct input next time!");
+
+    public static void customerData() {
+        String query = "SELECT CompanyName, ContactName, City, Country, Phone " +
+                "FROM Customers WHERE CustomerID = ? OR CompanyName LIKE ? " +
+                "ORDER BY Country";
+
+        ResultSet results = null;
+        PreparedStatement statement = null;
+        Connection connection = null;
+
+        try {
+            connection = DriverManager.getConnection(url, user, password);
+            statement = connection.prepareStatement(query);
+
+            statement.setInt(1, 1);
+            statement.setString(2, "%%");
+            results = statement.executeQuery();
+
+            System.out.println("CustomerID \t ContactName \t CompanyName \t City \t Country \t Phone");
+            System.out.println("-----------------------------------------------------------");
+            while (results.next()) {
+                String contactName = results.getString("ContactName");
+                String companyName = results.getString("CompanyName");
+                String city = results.getString("City");
+                String country = results.getString("Country");
+                String phone = results.getString("Phone");
+                System.out.println(contactName + "\t" + companyName + "\t" + city + "\t" + country + "\t" + phone);
+            }
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+        finally {
+            if (results != null) {
+                try {
+                    results.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (statement != null) {
+                try {
+                    statement.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 }
